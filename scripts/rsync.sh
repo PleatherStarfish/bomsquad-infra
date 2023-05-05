0f1af5f3908ad92d1eacb503e42c90c6bae998ab
Chris S <chris@soundshare.co.uk>
2023-05-05 19:17:49 +0100

    remove rysnc script - was for dev purposes only

--- scripts/rsync.sh
+++
@@ -1,6 +0,0 @@
-rsync -av /home/chris/temp/wiptemp/796/repos/bomsquad \
-  devops@134.209.65.8:/home/devops/app/dev/
-
-cd /home/chris/temp/wiptemp/796
-rsync -e "ssh -o 'ControlPath=$HOME/.ssh/ctl/%L-%r@%h:%p'" --relative -av $(git ls-files) \
-  devops@134.209.65.8:/home/devops/infra/
