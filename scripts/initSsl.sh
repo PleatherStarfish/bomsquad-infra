2b41c5b43fe1a3bfe99455740872329ce48e0701
Chris S <chris@soundshare.co.uk>
2025-04-17 18:43:31 +0100

    setup ssl addition of matomo

--- scripts/initSsl.sh
+++ scripts/initSsl.sh
@@ -4,9 +4,16 @@ sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
 sudo chown -R 101:101 /var/www/html
 
+# first
+sudo certbot certonly \
+  -d dev.bom-squad.com \
+  --webroot --webroot-path /var/www/html \
+  --agree-tos 
+
+# after
 sudo certbot certonly \
   -d bom-squad.com \
   -d dev.bom-squad.com \
+  -d matomo.bom-squad.com \
   --webroot --webroot-path /var/www/html \
-  --agree-tos \ 
-  --no-eff-email
+  --agree-tos 
